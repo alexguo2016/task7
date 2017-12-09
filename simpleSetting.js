5b506745c99d003f3993b7d7ff11f885c0117e1e
alexguo2016 <240791268@qq.com>
2017-12-09 23:53:46 +0800

    2017-12-9

---
+++ simpleSetting.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+	$(".tot-players").keydown(function(e){
+		var keynum
+		var keychar
+		var numcheck
+	
+		if(window.event) // IE
+		  {
+		  keynum = e.keyCode
+		  }
+		else if(e.which) // Netscape/Firefox/Opera
+		  {
+		  keynum = e.which
+		  }
+	
+		keychar = String.fromCharCode(keynum)
+		numcheck = /\d/
+	
+		return numcheck.test(keychar)
+	});
+	$(".tot-players").mouseleave(function(){
+		console.log("aaa");
+		var totalPlayers=parseInt($(".tot-players").val());
+		if(totalPlayers>18 || totalPlayers<6){
+		alert("游戏人数应该在6-18人！！");
+		return ;
+		}
+		var killerPlayers = 0,citizenPlayers = 0;
+		if(totalPlayers<9){
+			killerPlayers = 1;
+		}else if(totalPlayers>=9 && totalPlayers<=11){
+			killerPlayers = 2;
+		}else if(totalPlayers>=12 && totalPlayers<=15){
+			killerPlayers = 3;
+		}else if(totalPlayers>=16 && totalPlayers<=18){
+			killerPlayers = 4;
+		}
+		citizenPlayers = totalPlayers - killerPlayers;
+		$(".killerPlayers").text(killerPlayers);
+		$(".citizenPlayers").text(citizenPlayers);
+		
+		sessionStorage.setItem("total",$(".tot-players").val());
+		sessionStorage.setItem("killer",$(".killerPlayers").text());
+		sessionStorage.setItem("citizen",$(".citizenPlayers").text());
+	});
+	
+});
